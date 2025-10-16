b44c1032b37ade64e6664a2e95cfafa20d0dee4d
Naza2006 <nazario@uamv.edu.ni>
2025-10-15 23:09:44 -0600

    Estructura básica y validaciones iniciales

--- Eliminar_Tareas/Eliminar_Tareas/Program.cs
+++ Eliminar_Tareas/Eliminar_Tareas/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Eliminar_Tareas
+﻿using System.Threading;
+
+namespace Eliminar_Tareas
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (tareas.Count == 0)
+            {
+                Console.WriteLine("No hay tareas registradas.");
+                return;
+            }
+
+            ListarTareas();
+            Console.Write("\nIngrese el número de la tarea: ");
+            int num = int.Parse(Console.ReadLine());
+
+            }
         }
     }
 }
